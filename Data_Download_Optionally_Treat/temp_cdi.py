c8f22c141138e65340aea9ca52d0fe02e491425f
jmsjst22 <s1987119@baltic10>
2021-07-29 09:53:52 +0100

    add temp cdi

---
+++ Data_Download_Optionally_Treat/temp_cdi.py
@@ -0,0 +1,23 @@
+Variable:
+2m temperature
+Year:
+2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014
+Month:
+January, February, March, April, May, June, July, August, September, October, November, December
+Day:
+01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+Time:
+00:00, 01:00, 02:00, 03:00, 04:00, 05:00, 06:00, 07:00, 08:00, 09:00, 10:00, 11:00, 12:00, 13:00, 14:00, 15:00, 16:00, 17:00, 18:00, 19:00, 20:00, 21:00, 22:00, 23:00
+Sub-region extraction:
+North 4.3°, West 29°, South -2°, East 35.2°
+Format:
+NetCDF (experimental)
+Precipitation data can be acquired using this method and substituting the “Total precipitation” selection:
+Variable:
+Total precipitation
+Year:
+2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014
+Month:
+January, February, March, April, May, June, July, August, September, October, November, December
+Day:
+01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
