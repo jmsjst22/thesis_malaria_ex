7a5cc3d7795004510ce0a39fdf747a1a7b7613c4
jmsjst22 <74499643+jmsjst22@users.noreply.github.com>
2021-08-12 21:00:40 +0100

    Update base_model.py

--- random_forest/base_model.py
+++ random_forest/base_model.py
@@ -1,5 +1,3 @@
-#build the model
-
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import roc_auc_score, mean_squared_error #c stat
 import math
@@ -12,57 +10,59 @@ import pandas as pd
 from sklearn.inspection import permutation_importance
 import shap
 
-def evaluate(model, X_test, Y_test):
-    predictions = model.predict(X_test)
-    errors = abs(predictions - Y_test)
-    mape = 100 * np.mean(errors / Y_test)
-    accuracy = 100 - mape
-    print('Model Performance')
-    print('Average Error: {:0.4f} degrees.'.format(np.mean(errors)))
-    print('Accuracy = {:0.2f}%.'.format(accuracy))
-
-    return accuracy
-
-
+# Define directory
 filepath = ('/home/s1987119/Diss_data/Final/Final_Products/final_outputs/')
-plt.rc('font',size=8)
+# Read CSV
 data = pd.read_csv(filepath+'FINAL_RT_WO_ref_mr.csv')
-#Xcol1 = data.columns[data.columns.str.contains('hum')]
-#Xcol1 = data[Xcol1]
-#Xcol2 = Xcol1.columns[Xcol1.columns.str.contains('1mb')]
+# Select columns with all feature data in
 Xcol = data.columns[5:]
+# Select data from selected columns
 X = data[Xcol]
+# Select data for labels in machine learning (predicted variable)
 Y = data['me_rt_hh_district_prop']
+# Train, test split populations of data for training and testing the data - randomly.
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.25,random_state=42)
 
-RandomForestRegressor(random_state = 42)
+# Call RandomForestRegressor method
+rf = RandomForestRegressor(random_state = 42)
 
-
+# Train model with training populations of feature and label 
 rf_fit = rf.fit(X_train,Y_train)
 
+# Predict unseen Y from unseen X to test model performance
 Y_predict = rf_fit.predict(X_test)
 
+# Compute MSE for model error
 mse = mean_squared_error(Y_test,Y_predict)
+# mse --> rmse
 rmse = np.sqrt(mse)
 print(rmse)
 
+# Call tree explainer module for visualisation and analysis using Shapley Value
 explainer  = shap.TreeExplainer(rf_fit)
+# Get Shapley Values for each feature from X_test
 shap_values = explainer.shap_values(X_test)
 
+# Get mean Shapley value for all features (mean for each feature)
 vals= np.abs(shap_values).mean(0)
+# Convert to dataframe with associated feature/Shapley value pair
 feature_importance = pd.DataFrame(list(zip(X_train.columns,vals)),columns=['col_name','feature_importance_vals'])
+# Sort values by feature importance 
 feature_importance.sort_values(by=['feature_importance_vals'],ascending=False,inplace=True)
+# Print column heads and top 5 rows of feature improtance for shapley
 feature_importance.head()
 
-
+# Test perm_importance for difference (permutation importance for feature removal/addition accuracy test - not used in current study)
 perm_importance  = permutation_importance(rf_fit,X_test,Y_test)
 
+# Sort permutation importance values 
 sorted_idx = perm_importance.importances_mean.argsort()
-
 print(sorted_idx)
 
+# Plot bar graph of feature importances as measured by Shapley Value Mean
 plt.barh(X_train.columns[sorted_idx], perm_importance.importances_mean[sorted_idx])
 plt.show()
-
+# Plot summary plot of shapley values for all query points (not just mean)
 shap.summary_plot(shap_values,X_test, plot_type='bar')
+# Edit plot
 shap.summary_plot(shap_values,X_test, max_display = 10,  title='Shapley Values for Month Scale Environmental Factors', color_bar_label= 'Comparative Feature/Factor Value')
