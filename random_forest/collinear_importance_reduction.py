bc886ef449816d96c086ae005c846d38651c3d44
jmsjst22 <74499643+jmsjst22@users.noreply.github.com>
2021-08-12 21:32:59 +0100

    Update collinear_importance_reduction.py

--- random_forest/collinear_importance_reduction.py
+++ random_forest/collinear_importance_reduction.py
@@ -1,11 +1,10 @@
-#build the model
+# Experimental Collinear and unimportant feature removal not used in final product
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import roc_auc_score, mean_squared_error #c stat
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-#from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 import seaborn as sns
@@ -16,61 +15,50 @@ from sklearn.inspection import permutation_importance
 import shap
 from feature_selector import FeatureSelector
 
-fs = FeatureSelector
-
-def evaluate(model, X_test, Y_test):
-    predictions = model.predict(X_test)
-    errors = abs(predictions - Y_test)
-    mape = 100 * np.mean(errors / Y_test)
-    accuracy = 100 - mape
-    print('Model Performance')
-    print('Average Error: {:0.4f} degrees.'.format(np.mean(errors)))
-    print('Accuracy = {:0.2f}%.'.format(accuracy))
-
-    return accuracy
-
 
 filepath = ('/home/s1987119/Diss_data/Final/Final_Products/final_outputs/')
 plt.rc('font',size=8)
 data = pd.read_csv(filepath+'FINAL_RT_WO_ref_mr.csv')
-
-#Xcol1 = data.columns[data.columns.str.contains('temp')]
-#Xcol1 = data[Xcol1]
-#Xcol2 = Xcol1.columns[Xcol1.columns.str.contains('2mb')]
 Xcol = data.columns[5:]
 X = data[Xcol]
 Y = data['me_rt_hh_district_prop']
 
 
-
+# Call feature selector
 fs = FeatureSelector(data = X, labels = Y)
 
+# Identify collinear features with threshold of above 0.6 correlation coefficient.
 fs.identify_collinear(correlation_threshold=0.6)
 
 correlated_features = fs.ops['collinear']
-
+# Identify unimportant features
 fs.identify_zero_importance(task= 'regression',eval_metric = 'l2',n_iterations=10,early_stopping=True)
-
+# Identify Features that do not contribute to 0.95 of the importance 
 fs.identify_low_importance(cumulative_importance = 0.95)
-
+# Action low importance method
 low_importance_features = fs.ops['low_importance']
-
+# Remove low importance and collinear features
 X_nc_nli = fs.remove(methods = 'all')
 
 
-
+# Train/Test/Split with 
 X_train,X_test,Y_train,Y_test = train_test_split(X_nc_nli,Y,test_size=0.25,random_state=42)
+
 
 rf = RandomForestRegressor(random_state=42)
 
+# Train model
 rf_fit = rf.fit(X_train,Y_train)
 
+# Predict with test data
 Y_predict = rf_fit.predict(X_test)
 
+# Test with error, RMSE
 mse = mean_squared_error(Y_test,Y_predict)
 rmse = np.sqrt(mse)
 print(rmse)
 
+# Call visualiser/assessor of importance with Shapley value
 explainer  = shap.TreeExplainer(rf_fit)
 shap_values = explainer.shap_values(X_test)
 
